701f2fc2d64730eae1ad78acdc9ae82ecd2833a0
Sean 'Blitz' Homer <homer@therefore.ca>
2019-01-20 20:58:53 -0500

    Refreshing Code Base

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+readonly __gitPath=git@github.com:blitzcodes/content-player.git
+
+# abort on errors
+set -e
+
+git init
+git add -A
+git commit -m 'Refreshing Code Base'
+
+git push -f ${__gitPath} master:master
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'Refreshing Site'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f ${__gitPath} master:gh-pages
+
+cd -
+
